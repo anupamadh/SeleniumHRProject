01f15b6766fbd108f5e266192bc6a81644fdfba2
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-05-07 14:53:22 +0800

    added code for using database to get login info

--- src/test/java/hrapp/HomePage.java
+++ src/test/java/hrapp/HomePage.java
@@ -1,14 +1,14 @@
 package hrapp;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LoginPage;
 import resources.base;
-import utils.ExcelUtils;
+import utils.jdbcConnection;
 
 public class HomePage extends base{
 
@@ -17,7 +17,8 @@ public void initialize() throws IOException {
 	driver = initializeDriver();
 }
 
-@Test(dataProvider = "test1data")
+
+/**@Test(dataProvider = "test1data")
 public void basePageNavigation(String username, String password) throws IOException {
 	driver.get(prop.getProperty("url"));
 	LoginPage lp = new LoginPage(driver);
@@ -26,7 +27,21 @@ public void basePageNavigation(String username, String password) throws IOExcept
 	lp.getLoginButton().click();
 	
 }
+*/
 
+
+@Test
+public void basePageNavigation() throws IOException, SQLException {
+	driver.get(prop.getProperty("url"));
+	LoginPage lp = new LoginPage(driver);
+	jdbcConnection jd = new jdbcConnection();
+	jd.getConnection();
+	lp.getUsername().sendKeys(jd.getUsername());
+	lp.getPassword().sendKeys(jd.getPassword());
+	lp.getLoginButton().click();
+	
+}
+/**
 @DataProvider(name="test1data")
 public Object[][] getData() {
 	String excelPath = "/Users/anupamadhir/workspace/SeleniumHRProject/excel/data.xlsx";
@@ -49,5 +64,6 @@ public Object[][] testData(String excelPath, String sheetName) {
 	}
 	return data;
 }
+*/
 
 }
---
+++ src/test/java/utils/jdbcConnection.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class jdbcConnection {
+	String host="localhost";
+	String port= "3306";
+	String username = null;
+	String password = null;
+	public void getConnection() throws SQLException {
+	Connection con=DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/Qadbt", "root", "xxxx");
+	Statement s=con.createStatement();
+	ResultSet rs=s.executeQuery("select * from employee");
+	while(rs.next())
+	{
+		username = rs.getString("username");
+		password = rs.getString("user_password");
+	}
+	
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	
+}
